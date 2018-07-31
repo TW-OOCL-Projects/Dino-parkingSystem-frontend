977b5cc978d09e3dc8a6d8f3b15f3b29d523dd38
YAOCH2 <christine.s.yao@oocl.com>
2018-07-31 17:48:49 +0800

    [christine]login page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import 'antd'
 import WrappedNormalLoginForm from './components/Login'
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,44 +1,79 @@
-
-import React from 'react';
+import React, {Component} from 'react';
 //import ReactDOM from 'react-dom';
 import 'antd/dist/antd.css';
 import '../login.css';
-import { Form, Icon, Input, Button, Checkbox } from 'antd';
+import {Form, Icon, Input, Button, Checkbox} from 'antd';
 
 const FormItem = Form.Item;
 
- class NormalLoginForm extends React.Component {
+class Login extends Component {
+  render() {
+    const AntWrappedLoginForm = Form.create()(NormalLoginForm)
+    return (
+
+      <div className="container">
+        <div className="login-container">
+          <h1 className="page-title">Login</h1>
+          <div className="login-content">
+            <AntWrappedLoginForm onLogin={this.props.onLogin}/>
+          </div>
+        </div>
+      </div>
+
+    );
+  }
+}
+
+class NormalLoginForm extends React.Component {
   handleSubmit = (e) => {
     e.preventDefault();
-    this.props.form.validateFields((err, values) => {
-      if (!err) {
-        console.log('Received values of form: ', values);
-      }
-    });
+    this
+      .props
+      .form
+      .validateFields((err, values) => {
+        if (!err) {
+          console.log('Received values of form: ', values);
+        }
+      });
   }
- 
+
   render() {
-    const { getFieldDecorator } = this.props.form;
+    const {getFieldDecorator} = this.props.form;
     return (
       <Form onSubmit={this.handleSubmit} className="login-form">
         <FormItem>
           {getFieldDecorator('userName', {
-            rules: [{ required: true, message: 'Please input your username!' }],
+            rules: [
+              {
+                required: true,
+                message: 'Please input your username!'
+              }
+            ]
           })(
-            <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+            <Input
+              prefix={< Icon type = "user" style = {{ color: 'rgba(0,0,0,.25)' }}/>}
+              placeholder="Username"/>
           )}
         </FormItem>
         <FormItem>
           {getFieldDecorator('password', {
-            rules: [{ required: true, message: 'Please input your Password!' }],
+            rules: [
+              {
+                required: true,
+                message: 'Please input your Password!'
+              }
+            ]
           })(
-            <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+            <Input
+              prefix={< Icon type = "lock" style = {{ color: 'rgba(0,0,0,.25)' }}/>}
+              type="password"
+              placeholder="Password"/>
           )}
         </FormItem>
         <FormItem>
           {getFieldDecorator('remember', {
             valuePropName: 'checked',
-            initialValue: true,
+            initialValue: true
           })(
             <Checkbox>Remember me</Checkbox>
           )}
@@ -46,15 +81,12 @@ const FormItem = Form.Item;
           <Button type="primary" htmlType="submit" className="login-form-button">
             Log in
           </Button>
-          Or <a href="">register now!</a>
+          {/* Or
+          <a href="">register now!</a> */}
         </FormItem>
       </Form>
     );
   }
 }
 
-
- const WrappedNormalLoginForm = Form.create()(NormalLoginForm);
- export default WrappedNormalLoginForm;
-//  ReactDOM.render(<WrappedNormalLoginForm />, document.getElementById('container'));
-          
+export default Login;
